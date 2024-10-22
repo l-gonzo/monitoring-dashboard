51e05e22922c3df76d128b48845f2d76e9ddd72f
l-gonzo <9o.mndez@gmail.com>
2024-10-22 17:11:24 -0600

    db

---
+++ backend/SQL/dump.sql
@@ -0,0 +1,27 @@
+CREATE database system_monytor;
+
+use system_monytor;
+
+CREATE TABLE cpu_history (
+    id_log INT NOT NULL AUTO_INCREMENT,
+    login_user VARCHAR(50),
+    cpu_usage DECIMAL(5, 2),  -- Uso de CPU como porcentaje
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Fecha y hora del registro
+    PRIMARY KEY (id_log)
+);
+
+CREATE TABLE memory_history (
+    id_log INT NOT NULL AUTO_INCREMENT,
+    login_user VARCHAR(50),
+    memory_usage DECIMAL(5, 2),  -- Uso de CPU como porcentaje
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Fecha y hora del registro
+    PRIMARY KEY (id_log)
+);
+
+CREATE TABLE disk_history (
+    id_log INT NOT NULL AUTO_INCREMENT,
+    login_user VARCHAR(50),
+    disk_usage DECIMAL(5, 2),  -- Uso de CPU como porcentaje
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Fecha y hora del registro
+    PRIMARY KEY (id_log)
+);
