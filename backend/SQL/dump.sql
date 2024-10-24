8f7c4aaf81fec0a83932867e63187c5f055e521e
l-gonzo <9o.mndez@gmail.com>
2024-10-24 10:46:20 -0600

    v 1.0.9

--- backend/SQL/dump.sql
+++ backend/SQL/dump.sql
@@ -1,27 +1,76 @@
-CREATE database system_monytor;
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.9-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: system_monytor
+-- ------------------------------------------------------
+-- Server version	10.11.9-MariaDB
 
-use system_monytor;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE cpu_history (
-    id_log INT NOT NULL AUTO_INCREMENT,
-    login_user VARCHAR(50),
-    cpu_usage DECIMAL(5, 2),  -- Uso de CPU como porcentaje
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Fecha y hora del registro
-    PRIMARY KEY (id_log)
-);
+--
+-- Table structure for table `cpu_history`
+--
 
-CREATE TABLE memory_history (
-    id_log INT NOT NULL AUTO_INCREMENT,
-    login_user VARCHAR(50),
-    memory_usage DECIMAL(5, 2),  -- Uso de memoria como porcentaje
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Fecha y hora del registro
-    PRIMARY KEY (id_log)
-);
+DROP TABLE IF EXISTS `cpu_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cpu_history` (
+  `id_log` int(11) NOT NULL AUTO_INCREMENT,
+  `login_user` varchar(50) DEFAULT NULL,
+  `cpu_usage` decimal(5,2) DEFAULT NULL,
+  `timestamp` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_log`)
+) ENGINE=InnoDB AUTO_INCREMENT=8689 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE disk_history (
-    id_log INT NOT NULL AUTO_INCREMENT,
-    login_user VARCHAR(50),
-    disk_usage DECIMAL(5, 2),  -- Uso de disco como porcentaje
-    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,  -- Fecha y hora del registro
-    PRIMARY KEY (id_log)
-);
+--
+-- Table structure for table `disk_history`
+--
+
+DROP TABLE IF EXISTS `disk_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `disk_history` (
+  `id_log` int(11) NOT NULL AUTO_INCREMENT,
+  `login_user` varchar(50) DEFAULT NULL,
+  `disk_usage` decimal(5,2) DEFAULT NULL,
+  `timestamp` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_log`)
+) ENGINE=InnoDB AUTO_INCREMENT=8797 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `memory_history`
+--
+
+DROP TABLE IF EXISTS `memory_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `memory_history` (
+  `id_log` int(11) NOT NULL AUTO_INCREMENT,
+  `login_user` varchar(50) DEFAULT NULL,
+  `memory_usage` decimal(5,2) DEFAULT NULL,
+  `timestamp` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id_log`)
+) ENGINE=InnoDB AUTO_INCREMENT=8821 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-24 10:39:15
