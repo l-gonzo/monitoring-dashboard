2f018b7e4c04a37bf851f5c8d313bbcbc5dff37f
Jonh32 <rodriguezyoni32@outlook.com>
2024-10-29 13:20:47 -0600

    Test pull request

--- backend/api.php
+++ backend/api.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); 
 header("Access-Control-Allow-Headers: Content-Type"); 
 require_once 'connection.php'; 
-
+#Prueba de pull request
 $conn = new Connect();
 
 $action = $_GET['action'] ?? '';
